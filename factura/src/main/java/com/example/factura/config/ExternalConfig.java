2c59e3176c2346cb38f5b97f51c7ac8a52cd5cd8
Enzo1s <enziitoomartin@gmail.com>
2021-04-13 16:22:24 -0300

    Corrección error de conexión consul

---
+++ factura/src/main/java/com/example/factura/config/ExternalConfig.java
@@ -0,0 +1,10 @@
+package com.example.factura.config;
+
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Component;
+
+@RefreshScope
+@Component
+public class ExternalConfig {
+
+}
